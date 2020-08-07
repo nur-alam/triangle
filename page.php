71486206b57705c4f59bd71dcbc8578528abbeb8
nur alam <nuralam@nur.local>
2020-08-07 17:19:08 +0600

    page.php solved

--- page.php
+++ page.php
@@ -3,11 +3,11 @@
 echo 'page.php' . '</br></br>';
 
 if (have_posts()) :
-    //while (have_posts()) :
-        //the_post();
+    while (have_posts()) :
+        the_post();
         the_title();
         the_content();
-    //endwhile;
+    endwhile;
 endif;
 
 
